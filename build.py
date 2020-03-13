07c868af84e9bf9cd4a2a0b762a0d7ceb525c6db
Pedro Araújo <pedromcaraujo@gmail.com>
2020-03-13 09:45:00 +0000

    script to process docs folder

--- build.py
+++ build.py
@@ -1,11 +1,21 @@
 import os
+from pathlib import Path
 import markdown
 
 OUTPUT_FOLDER = os.path.join(os.getcwd(), "build")
+INPUT_FOLDER = os.path.join(os.getcwd(), "docs")
 
 if not os.path.exists(OUTPUT_FOLDER):
     os.mkdir(OUTPUT_FOLDER)
 
-html = markdown.markdownFromFile(
-    input="docs.md", output=os.path.join(OUTPUT_FOLDER, "docs.html"), encoding="UTF-8"
-)
+if not os.path.exists(INPUT_FOLDER):
+    os.mkdir(INPUT_FOLDER)
+
+with os.scandir(INPUT_FOLDER) as entries:
+    for entry in entries:
+        if entry.is_file():
+            output_filename = f"{Path(entry.name).stem}.html"
+            html = markdown.markdownFromFile(
+                input=entry.path, output=os.path.join(OUTPUT_FOLDER, output_filename),
+                encoding="UTF-8"
+            )
